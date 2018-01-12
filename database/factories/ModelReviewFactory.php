173c98d23c3c13ad518a6792228617f29c02f596
afd <assamforest9@gmail.com>
2018-01-12 15:23:14 +0530

    Seeding for Product and Review

--- database/factories/ModelReviewFactory.php
+++ database/factories/ModelReviewFactory.php
@@ -1,9 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
-
+use App\Model\Product;
 $factory->define(App\Model\Review::class, function (Faker $faker) {
     return [
-        //
+        'product_id'=>Product::all()->random(),
+        'customer'=>$faker->word,
+        'review'=>$faker->text,
+        'star'=>$faker->numberBetween(0,5)
     ];
 });
